d161e6d1dff41cd09e5ef055c9c025e5c28186a0
Oshroda <davisjosh2@gmail.com>
2017-02-25 23:27:05 -0800

    My work in Unity
    Segmented floors and walls with some assets, lights and textures.

---
+++ PuzzleHouse/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+  public GameObject player;
+  public float offsetValue;
+  private Vector3 offset;
+  private BoxCollider2D bc2d;
+
+	void Start ()
+  {
+    bc2d = GetComponent<BoxCollider2D>();
+    offset = this.transform.position - player.transform.position;
+	}
+	
+	void LateUpdate ()
+  {
+    this.transform.position = player.transform.position + offset;
+  }
+}
---
+++ PuzzleHouse/Assets/Scripts/QuadrantController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuadrantController : MonoBehaviour {
+  private BoxCollider2D bc2d;
+  private SpriteRenderer sr;
+
+
+	// Use this for initialization
+	void Start ()
+  {
+    bc2d = GetComponent<BoxCollider2D>();
+    sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+  {
+		
+	}
+
+  private void OnTriggerEnter2D(Collider2D collision)
+  {
+    if (collision.gameObject.name == "Vision")
+    {
+      //Debug.Log("enter");
+    }
+  }
+
+  private void OnTriggerExit2D(Collider2D collision)
+  {
+    if (collision.gameObject.name == "Vision")
+    {
+      Debug.Log("exit");
+      sr.color = Color.blue;
+      }
+  }
+}
